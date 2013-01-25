3529397803a806d42f6ef552964acfba618e7fc4
Radek Slupik <daknok@icloud.com>
2013-01-25 12:29:37 +0100

    Set correct speeds before starting games

--- Game1.java
+++ Game1.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class Game1 extends Buttons {
     public void act() {
         if (Greenfoot.mouseClicked(this)) {
+            Greenfoot.setSpeed(50);
             Greenfoot.setWorld(new FloodWorldMenu());
         }
     }
--- Game3.java
+++ Game3.java
@@ -3,6 +3,7 @@ import greenfoot.*;
 public class Game3 extends Buttons {
     public void act() {
         if (Greenfoot.mouseClicked(this)) {
+            Greenfoot.setSpeed(55);
             Greenfoot.setWorld(new ControlroomMenu());
         }
     }
